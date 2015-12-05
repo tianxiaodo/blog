f2f12f317745b1ae6c0370278e5cd38794acad3a
tqd <winter5400@sina.com>
2015-12-05 18:40:04 +0800

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var articles = require('./routes/article');
+              require("./db");
 
 var app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,20 @@ router.get('/reg', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render("user/login");
 });
+
+router.post('/login', function(req, res, next) {
+  var user =  req.body;
+  var m=new Model('User');
+  m.findOne(user,function(err,doc){
+    if(err){
+      res.redirect('/users/login');
+    }
+    else{
+      res.redirect('/article/add');
+    }
+  })
+});
+
 router.get('/logout', function(req, res, next) {
   res.send('退出');
 });
